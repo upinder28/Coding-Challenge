607f1e3b9bcb189148f5ecf43ba0c2074dd4413e
Upinder Kaur <kaurupinder28@gmail.com>
2024-10-20 00:07:30 +0530

    Add files via upload

---
+++ Day-1 (Lect-1,2,3) (Rohit Negi).cpp
@@ -0,0 +1,66 @@
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     cout << "Hello!" << endl;
+//     cout << "Welcome to my 1st code" << endl;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     cout << 3+2 << endl;
+//     cout << "3+2" << endl;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int a = 5;
+//     cout << a;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int a = 20;
+//     int b = 10;
+//     cout << a + b;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     char C = 'c';
+//     char B = 'b';
+//     cout << C << endl;
+//     cout << B << endl;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     float f = 2.1;
+//     float c = 1.1;
+//     cout << f << endl;
+//     cout << c << endl;
+//     cout << f + c << endl;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     double d = 2.12;
+//     double c = 1.12;
+//     cout << d << endl;
+//     cout << c << endl;
+//     cout << d + c << endl;
+// }
+
+#include<iostream>
+using namespace std;
+int main() {
+    bool b = true;
+    bool c = false;
+    cout << b << endl;
+    cout << c << endl;
+}
